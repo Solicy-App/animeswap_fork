4ac33e2a3eb2c944860833e936e49c7362c78e86
SamTop-Solicy <samvel.topuzyan@solicy.net>
2022-10-25 01:15:07 +0400

    Fix

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,21 +1,21 @@
 import { NetworkType } from '@animeswap.org/v1-sdk'
 
 export enum SupportedChainId {
-  APTOS = 34,
+  APTOS = 1,
   APTOS_TESTNET = 2,
-  APTOS_DEVNET = 1,
+  APTOS_DEVNET = 34,
 }
 
 export const CHAIN_IDS_TO_NAMES = {
-  [SupportedChainId.APTOS]: 'aptos_devnet',
+  [SupportedChainId.APTOS]: 'aptos',
   [SupportedChainId.APTOS_TESTNET]: 'aptos_testnet',
-  [SupportedChainId.APTOS_DEVNET]: 'aptos',
+  [SupportedChainId.APTOS_DEVNET]: 'aptos_devnet',
 }
 
 export const CHAIN_IDS_TO_SDK_NETWORK = {
-  [SupportedChainId.APTOS]: NetworkType.Devnet,
+  [SupportedChainId.APTOS]: NetworkType.Mainnet,
   [SupportedChainId.APTOS_TESTNET]: NetworkType.Testnet,
-  [SupportedChainId.APTOS_DEVNET]: NetworkType.Mainnet,
+  [SupportedChainId.APTOS_DEVNET]: NetworkType.Devnet,
 }
 
 export function isSupportedChain(chainId: number | undefined): chainId is SupportedChainId {
